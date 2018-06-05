3e7bdc60e20eb0255995d083f9f1cff70390cc79
Manoj Panta <manojpanta95@gmail.com>
2018-06-05 09:29:22 -0600

    passing test for creating new genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,7 +2,11 @@ class GenresController < ApplicationController
 
   def index
     @genres = Genre.all
+    @genre = Genre.new
   end
 
-
+  def create
+    @genre = Genre.create(params[:name])
+    redirect_to genres_path
+  end
 end
--- spec/features/admin_visits_genre_index_page_spec.rb
+++ spec/features/admin_visits_genre_index_page_spec.rb
@@ -15,5 +15,23 @@ describe "admin visits genre index index page" do
       expect(page).to have_content(genre.name)
       expect(page).to have_content(genre1.name)
     end
+    it "can see create new genre link and can create new genre " do
+      admin = User.create(username: "Dee", password: "password", role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      genre = Genre.create(name: 'scifi')
+
+      visit genres_path
+
+      expect(page).to have_content('Create New Genre')
+
+      fill_in 'genre[name]', with: 'GENRE'
+      click_on 'Create Genre'
+
+      expect(current_path).to eq(genres_path)
+      expect(page).to have_content(Genre.last.name)
+      expect(page).to have_content(genre.name)
+    end
   end
 end
